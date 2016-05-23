5891bba5c7a1150e64daaee8e2ee266be119aa94
Nojes <vv.nojenko@gmail.com>
2016-05-23 10:17:01 +0300

    app prototype

--- Lesson.04-PayrollControls/main.cpp
+++ Lesson.04-PayrollControls/main.cpp
@@ -4,6 +4,12 @@
 
 // Global Variables
 
+// Edits
+HWND hEditAdd, hEditResult;
+// Buttons
+HWND hBtnAdd, hBtnDel, hBtnClear, hBtnCalc;
+// Lists
+HWND hList;
 
 BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpszCmdLine, int nCmdShow)
@@ -17,7 +23,13 @@ BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 	switch (message)
 	{
 	case WM_INITDIALOG:
+		hEditAdd = GetDlgItem(hDlg, IDC_EDIT_ADD);
+		hEditResult = GetDlgItem(hDlg, IDC_EDIT_RESULT);
 
+		hBtnAdd = GetDlgItem(hDlg, IDC_EDIT_ADD);
+		hBtnDel = GetDlgItem(hDlg, IDC_BTN_DEL);
+		hBtnClear = GetDlgItem(hDlg, IDC_BTN_CLEAR);
+		hBtnCalc = GetDlgItem(hDlg, IDC_BTN_CALC);
 		break;
 
 	case WM_COMMAND:
