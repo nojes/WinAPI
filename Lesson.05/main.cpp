a3650391bdc2790d1412432c9fb809ab8202b849
Nojes <vv.nojenko@gmail.com>
2016-05-25 10:07:59 +0300

    init dialog

--- Lesson.05/main.cpp
+++ Lesson.05/main.cpp
@@ -3,7 +3,16 @@
 
 #include "resource.h"
 
-
+// ComboBoxes
+HWND hComboCategory;
+// List's
+HWND hListCatalog, hListBasket;
+// Button's
+HWND hBtnEditItem, hBtnAddItem, hBtnClearBasket, hBtnDelBasketItem, hBtnSubmit;
+// Edit's
+HWND hSum;
+// Check's
+HWND hCheckSaveReport;
 
 
 BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
@@ -18,7 +27,21 @@ BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 	switch (message)
 	{
 	case WM_INITDIALOG:
-
+		// ComboBoxes
+		hComboCategory = GetDlgItem(hDlg, IDC_COMBO_CATEGORY);
+		// List's
+		hListBasket = GetDlgItem(hDlg, IDC_LIST_BASKET);
+		hListCatalog = GetDlgItem(hDlg, IDC_LIST_CATALOG);
+		// Button's
+		hBtnAddItem = GetDlgItem(hDlg, IDC_BTN_ADD_ITEM);
+		hBtnClearBasket = GetDlgItem(hDlg, IDC_BTN_CLEAR_BASKET);
+		hBtnDelBasketItem = GetDlgItem(hDlg, IDC_BTN_DEL_BASKET_ITEM);
+		hBtnEditItem = GetDlgItem(hDlg, IDC_BTN_EDIT_ITEM);
+		hBtnSubmit = GetDlgItem(hDlg, IDC_BTN_SUBMIT);
+		// Edit's
+		hSum = GetDlgItem(hDlg, IDC_SUM);
+		// Check's
+		hCheckSaveReport = GetDlgItem(hDlg, IDC_CHECK_SAVE_RERT);
 		break;
 
 	case WM_COMMAND:
