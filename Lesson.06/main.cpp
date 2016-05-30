0750ae727cabfab35d4e55b140bb06b72a6c39e2
Nojes <vv.nojenko@gmail.com>
2016-05-30 09:18:19 +0300

    init lesson #6

---
+++ Lesson.06/main.cpp
@@ -0,0 +1,36 @@
+#include <Windows.h>
+#include <cstdio>
+
+#include "resource.h"
+
+/**
+ *	"Simple Gallery"
+ */
+
+
+
+BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpszCmdLine, int nCmdShow)
+{
+	return DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), nullptr, DlgProc);
+}
+
+
+BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+
+		break;
+
+	case WM_COMMAND:
+
+		break;
+
+	case WM_CLOSE:
+		EndDialog(hDlg, 0);
+		return TRUE;
+	}
+	return FALSE;
+}
