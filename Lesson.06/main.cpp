c5cc6256de283f968f46deec48e594ccdaa030a3
Nojes <vv.nojenko@gmail.com>
2016-05-30 09:18:19 +0300

    init dialog

--- Lesson.06/main.cpp
+++ Lesson.06/main.cpp
@@ -7,7 +7,15 @@
  *	"Simple Gallery"
  */
 
-
+// bitmap's
+const int BMP_COUNT = 5;
+HBITMAP hBmp[BMP_COUNT];
+// list's
+HWND hList;
+// static's
+HWND hStaticBmp;
+// button's
+HWND hBtnPrev, hBtnNext, hBtnStart, hBtnEnd;
 
 BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
 int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpszCmdLine, int nCmdShow)
@@ -15,13 +23,22 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpszCmdLine, int 
 	return DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), nullptr, DlgProc);
 }
 
-
 BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
 	switch (message)
 	{
 	case WM_INITDIALOG:
+		hList = GetDlgItem(hDlg, IDC_LIST);
+		hStaticBmp = GetDlgItem(hDlg, IDC_STATIC_BMP);
+		hBtnPrev = GetDlgItem(hDlg, IDC_BTN_PREV);
+		hBtnNext = GetDlgItem(hDlg, IDC_BTN_NEXT);
+		hBtnStart = GetDlgItem(hDlg, IDC_BTN_START);
+		hBtnEnd = GetDlgItem(hDlg, IDC_BTN_END);
 
+		// get bitmap's
+		for (size_t i = 0; i < BMP_COUNT; i++) {
+			hBmp[i] = LoadBitmap(GetModuleHandle(0), MAKEINTRESOURCE(IDR_JPG1+i));
+		}
 		break;
 
 	case WM_COMMAND:
