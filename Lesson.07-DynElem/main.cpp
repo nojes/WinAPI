48faa6f689dda587e0f6ddbe38ab5a5d4c0e8d0e
Nojes <vv.nojenko@gmail.com>
2016-06-01 09:57:50 +0300

    init dialog

--- Lesson.07-DynElem/main.cpp
+++ Lesson.07-DynElem/main.cpp
@@ -10,6 +10,12 @@
 *	"Dynamic Controls example"
 */
 
+// StaticText's
+HWND hStaticPer;
+// Button's
+HWND hBtnStart, hBtnStop, hBtnReset;
+// ProgressBar's
+HWND hProgress;
 
 
 BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
@@ -23,7 +29,18 @@ BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 	switch (message)
 	{
 	case WM_INITDIALOG:
+		hStaticPer = GetDlgItem(hDlg, IDC_STATIC_PER);
+		hBtnStart = GetDlgItem(hDlg, IDC_BTN_START);
+		hBtnStop = GetDlgItem(hDlg, IDC_BTN_STOP);
+		hBtnReset = GetDlgItem(hDlg, IDC_BTN_RESET);
+		hProgress = GetDlgItem(hDlg, IDC_PROGRESS);
 
+		// set ProgressBar background
+		SendMessage(hProgress, PBM_SETBKCOLOR, 0, LPARAM(RGB(128, 128, 128)));
+		// set Progress color
+		SendMessage(hProgress, PBM_SETBARCOLOR, 0, LPARAM(RGB(128, 255, 128)));
+		// set Progress position
+		SendMessage(hProgress, PBM_SETPOS, 50, 0);
 		break;
 
 	case WM_COMMAND:
