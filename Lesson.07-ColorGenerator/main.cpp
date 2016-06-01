b25fba1601a7cc418fa18d156dbd85212ad9020e
Nojes <vv.nojenko@gmail.com>
2016-06-01 11:01:48 +0300

    init lesson #7: example 2

---
+++ Lesson.07-ColorGenerator/main.cpp
@@ -0,0 +1,46 @@
+#include <Windows.h>
+#include <cstdio>
+#include <CommCtrl.h>
+
+#include "resource.h"
+
+#pragma comment(lib, "comctl32")
+
+/**
+*	"Dynamic Controls example 2"
+*/
+
+// StaticText's
+
+// ProgressBar's
+
+
+
+BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpszCmdLine, int nCmdShow)
+{
+	return DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), nullptr, DlgProc);
+}
+
+BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_INITDIALOG:
+
+		break;
+
+	case WM_COMMAND:
+	
+		break;
+
+	case WM_TIMER:
+		
+		break;
+
+	case WM_CLOSE:
+		EndDialog(hDlg, 0);
+		return TRUE;
+	}
+	return FALSE;
+}
