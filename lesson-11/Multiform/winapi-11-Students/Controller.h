cff5a763b5c32dade7f59ea6e25853a8e9b870cf
nojes <vv.nojenko@gmail.com>
2016-06-15 09:45:25 +0300

    init lesson #11

---
+++ lesson-11/Multiform/winapi-11-Students/Controller.h
@@ -0,0 +1,20 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include "Header.h"
+
+class Controller
+{
+
+public:
+	Controller();
+	~Controller();
+	static Controller *ptr;
+
+	void Cls_OnClose(HWND hDlg);
+	virtual BOOL Cls_OnInitDialog(HWND hDlg	, HWND hDlgFocus, LPARAM lParam);
+	virtual void Cls_OnCommand(HWND hDlg, int ctl_ID, HWND ctl_Handle, UINT codeNotify);
+	static BOOL CALLBACK DlgProc(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam);
+};
+
+#endif // CONTROLLER_H
---
+++ lesson-11/Multiform/winapi-11-Students/Header.h
@@ -0,0 +1,13 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <Windows.h>
+#include <windowsx.h>
+#include <tchar.h>
+#include <CommCtrl.h>
+
+#include "resource.h"
+
+#pragma comment(lib, "comctl32")
+
+#endif // HEADER_H
